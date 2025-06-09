eca7e5506730f0eea7954f5829e08b22eb9606b8
yogeshwar bhardwaj <bhardwajyogeshwar686@gmail.com>
2025-06-09 11:56:02 +0530

    yogi

---
+++ PrimeChecker.java
@@ -0,0 +1,22 @@
+public class PrimeChecker {
+    public static boolean isPrime(int n) {
+        if (n <= 1) {
+            return false;
+        }
+        for (int i = 2; i < n; i++) {
+            if (n % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        int number = 29;
+        if (isPrime(number)) {
+            System.out.println(number + " is a prime number.");
+        } else {
+            System.out.println(number + " is not a prime number.");
+        }
+    }
+}
